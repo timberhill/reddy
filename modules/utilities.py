6712800a339df4eeffd538190cfb26257b4077e1
Max Timberhill <max.lisogorski@gmail.com>
2020-06-27 18:56:34 +0100

    updated load_posts() with better progress and ETA

--- modules/utilities.py
+++ modules/utilities.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import time
 
 from . import DataContext
@@ -49,7 +49,7 @@ def load_pushshift_post_ids(papi, subreddit_name, epochrange):
     return ids
 
 
-def send_request(request_function, retries=3, progress=True, wait=30):
+def send_request(request_function, retries=3, ignore_errors=False, wait=10):
     retry = 0
     result = None
     while retry <= retries:
@@ -58,10 +58,10 @@ def send_request(request_function, retries=3, progress=True, wait=30):
             break
         except Exception as e:
             retry += 1
-            if progress and retry == retries:
-                print(f"Error occured in API request:\n{e}\n\nSkipping.")
-            elif progress and retry <= retries:
-                print(f"Error occured in API request:\n{e}\n\nRetrying in {wait}s...")
+            if not ignore_errors and retry == retries:
+                print(f"\nError occured in API request:\n{e}\nSkipping.")
+            elif not ignore_errors and retry <= retries:
+                print(f"\nError occured in API request:\n{e}\nRetrying in {wait}s...")
                 time.sleep(wait)
 
     return result
@@ -81,50 +81,73 @@ def load_posts(subreddit_name, epochrange, papi, rapi, progress=True):
     epochrange, tuple: 'from' and 'to' epochs in unix time.
 
     """
+    if progress:
+        print(f"Downloading posts from r/{subreddit_name}...", end="\r")
+
+    # for the progress:
+    start = datetime.utcnow()
+    posts_loaded = 0
+    full_range = datetime.fromtimestamp(epochrange[0]) - datetime.fromtimestamp(epochrange[1])
+
     ids = []
-    epoch_diff = 1000 # how much unix time to skip if the API is stuck
+    epoch_diff = 3600 # how much unix time (seconds) to skip if the API is stuck
     oldest_epoch = epochrange[0]
     while oldest_epoch > epochrange[1]:
         # load the IDs
-        if progress:
-            print("> fetching pushshift", end="", flush=True)
         ps_posts = send_request(
             lambda: papi.search(subreddit_name, before=int(oldest_epoch), limit=500),
-            retries=5, progress=progress
+            retries=5,
+            ignore_errors=True
         )
-        if progress:
-            print(f" [{len(ps_posts)}]", end="", flush=True)
 
         if len(ps_posts) == 0:
             oldest_epoch += epoch_diff
             continue
 
         ps_created_utc = [post.created_utc for post in ps_posts]
-        epoch_diff = max(ps_created_utc) - min(ps_created_utc)
+        # epoch_diff = max(ps_created_utc) - min(ps_created_utc)
 
+        # prepare post ID subsets for reddit API
         ids = [f"t3_{post.id}" for post in ps_posts]
         n = 100 # number of post ids per request (redit api limitation)
         id_subsets = [ids[i*n : (i+1)*n] for i in range((len(ids)+n-1)//n)]
 
         # load the posts from Reddit API
-        if progress:
-            print(f", fetching reddit..", end="", flush=True)
-        with DataContext() as datacontext:
-            for i, subset in enumerate(id_subsets):
-                posts = send_request(
-                    lambda: rapi.info(subreddit_name, subset),
-                    retries=5, progress=progress
-                )
-                if progress:
-                    print(f".{i+1}", end="", flush=True)
+        for i, subset in enumerate(id_subsets):
+            posts = send_request(
+                lambda: rapi.info(subreddit_name, subset),
+                retries=5,
+                ignore_errors=True
+            )
+            if posts is None:
+                continue
 
-                if posts is None:
-                    continue
-
+            with DataContext() as datacontext:
                 datacontext.add_posts(posts)
+                datacontext.commit()
 
-                oldest_epoch = min([post.created_utc for post in posts])
+            oldest_epoch = min([post.created_utc for post in posts])
+            posts_loaded += len(posts)
 
-            datacontext.commit()
-            if progress:
-                print(f", oldest: {datetime.fromtimestamp(oldest_epoch)}")
+        if progress:
+            # calculate ETA
+            loaded_range = datetime.fromtimestamp(epochrange[0]) - datetime.fromtimestamp(oldest_epoch)
+            time_since_start = datetime.utcnow() - start
+            load_rate = loaded_range.total_seconds() / (time_since_start.total_seconds()) # seconds of posts downloded per second
+            percentage = 100.0 * loaded_range.total_seconds() / full_range.total_seconds()
+            eta_sec = (full_range - loaded_range).total_seconds() / load_rate
+            m, s = divmod(eta_sec, 60)
+            h, m = divmod(m, 60)
+
+            print(
+                f" [r/{subreddit_name.lower()}] " + \
+                f"{percentage:.1f}% " + \
+                f"Posts: {posts_loaded/1000:.1f}k. " + \
+                f"Oldest: {datetime.fromtimestamp(oldest_epoch)}. " + \
+                f"ETA: {h:0.0f}h {m:0.0f}m {s:0.0f}s",
+            end='\r')
+
+    print(
+        f"[r/{subreddit_name}] " + \
+        f"Downloaded {posts_loaded} posts in {time_since_start}                           "
+    )
